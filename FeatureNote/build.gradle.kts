210e04326b2746bc5c80e303654f36aaf65995c1
domjos1994 <developing@domjos.de>
2024-11-07 15:45:39 +0100

    add modules

---
+++ FeatureNote/build.gradle.kts
@@ -0,0 +1,72 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kapt.library)
+    alias(libs.plugins.hilt.library)
+}
+
+android {
+    namespace = "de.dojodev.mybiblenotes.feature.note"
+    compileSdk = rootProject.extra["sdk_compile"] as Int
+
+    defaultConfig {
+        minSdk = rootProject.extra["sdk_min"] as Int
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = rootProject.extra["minify"] as Boolean
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = rootProject.extra["java_jvm"] as JavaVersion
+        targetCompatibility = rootProject.extra["java_jvm"] as JavaVersion
+    }
+    kotlinOptions {
+        jvmTarget = rootProject.extra["java_version"] as String
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
+    }
+}
+
+dependencies {
+    // projects
+    implementation(project(":Data"))
+    implementation(project(":ComposeComponents"))
+
+    // hilt
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.hilt.android)
+
+    // core
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+
+    // compose
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // testing dependencies
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+}
